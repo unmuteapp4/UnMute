cd98754d1aa9f42b92e6a48666e8b0548cbe5fa3
unmuteapp4 <unmuteapp4@gmail.com>
2017-01-08 01:46:18 +0530

    New features added 1

--- app/src/main/java/com/unmuteapp4gmail/unmute/login.java
+++ app/src/main/java/com/unmuteapp4gmail/unmute/login.java
@@ -1,7 +1,15 @@
 package com.unmuteapp4gmail.unmute;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewStub;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class login extends AppCompatActivity {
 
@@ -10,4 +18,36 @@ public class login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void sendMessage4(String activity) {
+        Intent intent = new Intent(this, String.class);
+        startActivity(intent);
+    }
+
+    public void onClick(View v) {
+        final int id = v.getId();
+        switch (id) {
+            case R.id.btnSubmit:
+
+                Spinner mySpinner = (Spinner) findViewById(R.id.spinner1);
+                String text = mySpinner.getSelectedItem().toString();
+                if (text.equals("Volunteer")) {
+                   sendMessage4("Volunteer");
+                }
+
+                else if (text.equals("Student")) {
+                  sendMessage4("Student");
+               }
+
+                else {
+                    sendMessage4("Admin");
+                }
+
+                break;
+        }
+
+    }
+
 }
+
+
