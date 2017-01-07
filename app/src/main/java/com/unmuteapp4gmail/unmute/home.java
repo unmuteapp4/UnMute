4a5fb702c289a6d6de9743ceb7fc31683866fa61
unmuteapp4 <unmuteapp4@gmail.com>
2017-01-07 23:33:09 +0530

    New features added

--- app/src/main/java/com/unmuteapp4gmail/unmute/home.java
+++ app/src/main/java/com/unmuteapp4gmail/unmute/home.java
@@ -1,7 +1,9 @@
 package com.unmuteapp4gmail.unmute;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class home extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void sendMessage1(View view) {
+        Intent intent = new Intent(this, login.class);
+        startActivity(intent);
+    }
+
+    public void sendMessage2(View view) {
+        Intent intent = new Intent(this, Donate.class);
+        startActivity(intent);
+    }
+
+    public void sendMessage3(View view) {
+        Intent intent = new Intent(this, About.class);
+        startActivity(intent);
     }
 }
